ce817880d6527f1b0f7a4caa0a3f050a45abce4d
KotovsAndrejs <125635760+KotovsAndrejs@users.noreply.github.com>
2024-03-20 13:16:12 +0200

    Update 1_city_info.py

--- 1_city_info.py
+++ 1_city_info.py
@@ -12,23 +12,27 @@ import json
 def get_city_information(city):
     link = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=3&language=en&format=json"
     
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(link) as response:
         data = response.read().decode('utf-8')
 
     return json.loads(data)
 
 def display_city_information(city_info):
-    if city_info
+    if city_info:
         print("City Information:")
         for city_data in city_info['results']:
             print(f"Name: {city_data['name']}")
-            print(f"Country: {city_data['county']}")
-            print(f"Latitude: {city_data['lat']}")
-            print(f"Longitude: {city_data['lon']}")
+            print(f"Country: {city_data['country']}")
+            print(f"Latitude: {city_data['latitude']}")
+            print(f"Longitude: {city_data['longitude']}")
+            if 'population' in city_data:
+                print(f"Population: {city_data['population']}")
+            else:
+                print(f"Population: unknown")
             print("-----------------------")
     else:
         print("No city information available.")
 
 city_name = input("Enter city name: ")
-city_information = get_city_information(city_name)
-display_city_information(city_information)
+city_info = get_city_information(city_name)
+display_city_information(city_info)
