fb16518d6382fcbaff5a3bac994239c854be9562
KotovsAndrejs <125635760+KotovsAndrejs@users.noreply.github.com>
2024-03-20 13:16:47 +0200

    Update 2_location_weather.py

--- 2_location_weather.py
+++ 2_location_weather.py
@@ -4,3 +4,27 @@
 # izmantojot sekojošu datu linku 
 # https://api.open-meteo.com/v1/forecast?latitude=56.8&longitude=24.2&hourly=temperature_2m,precipitation&forecast_days=1
 # 
+import urllib.request
+import json
+
+def get_weather_information(time):
+    link = f"https://geocoding-api.open-meteo.com/v1/search?name={time}&count=3&language=en&format=json"
+    
+    with urllib.request.urlopen(link) as response:
+        data = response.read().decode('utf-8')
+
+    return json.loads(data)
+
+def display_weather_information(weather_info):
+    if weather_info:
+        print("Weather Information:")
+        for weather_data in weather_info['results']:
+            print(f"Time: {weather_data['time']}")
+            print(f"Temperature: {weather_data['country']}")
+            print(f"Precipitation: {weather_data['precipitation']}")
+            print("-----------------------")
+    else:
+        print("No city information available.")
+        
+weather_info = get_weather_information()
+display_weather_information(weather_info)
